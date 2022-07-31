ac1aba657870f0c8d824131760b6110cb13bbcff
xingpeng <2429727684@qq.com>
2022-07-31 21:56:29 +0800

    fix: layotto preview server

--- layotto/layotto-with-bookinfo/init/foreground.sh
+++ layotto/layotto-with-bookinfo/init/foreground.sh
@@ -2,6 +2,7 @@ FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done;
 docker pull layotto/preview_server:latest
 docker run -d -p 9080:8080 --name layotto-preview-server layotto/preview_server:latest
 git clone https://github.com/layotto/istio.git
+pwd
 echo "export PATH=/root/istio/bin:\$PATH" >> .bashrc
 export PATH=/root/istio/bin:$PATH
 istioctl install --set profile=demo -y --manifests=/root/istio/manifests --set .values.global.proxy.image="layotto/proxyv2:latest" --set meshConfig.defaultConfig.binaryPath="/usr/local/bin/mosn"
