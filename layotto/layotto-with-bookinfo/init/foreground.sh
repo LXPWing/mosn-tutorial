870be94d343cd3de85ec1e1ccafbd7786db15f6d
xingpeng <2429727684@qq.com>
2022-07-31 22:16:40 +0800

    fix: layotto preview server

--- layotto/layotto-with-bookinfo/init/foreground.sh
+++ layotto/layotto-with-bookinfo/init/foreground.sh
@@ -1,8 +1,13 @@
 FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
+export ISTIO_VERSION=1.10.6
 docker pull layotto/preview_server:latest
 docker run -d -p 9080:8080 --name layotto-preview-server layotto/preview_server:latest
 git clone https://github.com/layotto/istio.git
-ls
-echo "export PATH=/root/istio/bin:\$PATH" >> .bashrc
-export PATH=/root/istio/bin:$PATH
-istioctl install --set profile=demo -y --manifests=/root/istio/manifests --set .values.global.proxy.image="layotto/proxyv2:latest" --set meshConfig.defaultConfig.binaryPath="/usr/local/bin/mosn"
+
+curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
+
+cp /root/istio/bookinfo /root/istio-${ISTIO_VERSION}
+
+echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
+export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
+istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests --set .values.global.proxy.image="layotto/proxyv2:latest" --set meshConfig.defaultConfig.binaryPath="/usr/local/bin/mosn"
